09be4df414666575dc7078f1d0d0292f62c4ecf2
Exequiel Mleziva <exequiel@hyan.dev>
2025-04-01 17:54:55 -0300

    chore: Phase 3 - Visual polish

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,16 +9,16 @@ const handleClick = () => {
 }
   
   return (
-    <li key={course.id} title='Click to favorite'>
+    <li key={course.id} title='Click to favorite' >
     
-    <button aria-label='Click to favorite' onClick={() => handleClick()} className={classNames('flex flex-row gap-3 rounded-lg bg-slate-700 border-2 p-2 cursor-pointer  transition-colors', {
+    <button aria-label='Click to favorite' onClick={() => handleClick()} className={classNames('w-full flex flex-row gap-3 rounded-lg bg-slate-700 border-2 p-2 cursor-pointer transition-colors', {
       'border-slate-600 hover:border-slate-400': !course.favorite && !isPending ,
       'border-yellow-300 hover:border-yellow-100': course.favorite && !isPending,
       'border-gray-400': isPending
     })} >
-    <img src={course.instructor_image_url} alt={course.instructor_name} className='rounded-lg'/>
+    <img src={course.instructor_image_url} alt={course.instructor_name} className='rounded-lg size-30'/>
     
-    <div className='flex flex-row justify-between w-full'>
+    <div className='flex flex-row w-full text-left'>
       <div>
         <h2 className='text-lg font-bold'>{course.instructor_name}</h2>
         <p className='italic'>{course.title}</p>
